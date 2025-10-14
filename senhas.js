e770e1b51bc6c9816d126e008fc907e3c4d147c8
auxpainel <regdzin@gmail.com>
2025-10-14 11:52:08 -0300

    Atualizar o senhas.js

--- senhas.js
+++ senhas.js
@@ -5,6 +5,7 @@ window.verificarSenha = function(senha) {
         "rick",
         "013179",
         "1537",//10do11de2025
+        "neyney10",//21do10do2025
         "#Neymar10",//17do102025
         "FLASH1K_"//19do10dw2025
     ];
