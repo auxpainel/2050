1d2c7d95c7b391c49d9205609703ddda43638c72
auxpainel <regdzin@gmail.com>
2025-11-03 14:16:50 -0300

    Atualizar o senhas.js

--- senhas.js
+++ senhas.js
@@ -1,13 +1,7 @@
 // senhas.js - Nova versão corrigida
 window.verificarSenha = function(senha) {
     const senhasValidas = [
-        "tainara",
-        "rick",
-        "013179",
-        "#Neymar10",//0401102025
-        "1537",//10do11de2025
-        "Mkff0000",//08do11
-        "lacerda22"//29d11
+        "cocodomiguel"
 ];
     // Verifica a senha exatamente como digitada (case sensitive)
     return senhasValidas.includes(senha);
