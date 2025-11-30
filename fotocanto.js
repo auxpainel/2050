68d83c3580805ef0a84256a162f9d6ef07790e7a
auxpainel <regdzin@gmail.com>
2025-11-30 19:06:56 -0300

    Criar fotocanto.js

---
+++ fotocanto.js
@@ -0,0 +1,3 @@
+// fotocanto.js
+// Apenas define a URL da imagem que o script principal irá usar.
+window.fotoCantoURL = 'https://raw.githubusercontent.com/auxpainel/2050/refs/heads/main/Natal.png';
